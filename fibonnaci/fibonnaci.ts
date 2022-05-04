61a5c08c3c9f12a04d89f036a4144c7a4fdcb6d9
cossy1 <onyekwuocosmas@gmail.com>
2022-05-04 17:07:25 +0100

    added fibonnacci sequence function

---
+++ fibonnaci/fibonnaci.ts
@@ -0,0 +1,14 @@
+//fibonacci sequence function
+
+function fibonacci (n){
+ let arr = [0, 1];
+
+ for(let i = 2; i <= n; i++){
+     arr.push(arr[i - 2] + arr[i - 1]);
+ }
+
+return arr[n];
+
+}
+
+console.log(fibonacci(4));
