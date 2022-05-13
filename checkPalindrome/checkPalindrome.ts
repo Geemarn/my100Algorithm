adfb0618ebc6df375815e515e8dc59aa47891275
cossy1 <onyekwuocosmas@gmail.com>
2022-05-13 11:39:12 +0100

    added palindrome

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -1,7 +1,26 @@
-function checkPalindrome(inputString: string): boolean {
+// function checkPalindrome(str) {
+//     let re = /[^A-Za-z0-9]/g;
+//     str = str.toLowerCase().replace(re, '');
+//     let len = str.length;
+//     for (let i = 0; i < len/2; i++) {
+//         if (str[i] !== str[len - 1 - i]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+function checkPalindrome(str) {
+    let regex = /[\W_/]/g;
+    let newStr = str.toLowerCase().replace(regex, '');
+    let reversed = newStr.split('').reverse().join('');
+    return reversed === newStr;
 
 }
+
+
 
 console.log(checkPalindrome('aabaa'));
 console.log(checkPalindrome('abac'));
 console.log(checkPalindrome('a'));
+console.log(checkPalindrome('1231'));
